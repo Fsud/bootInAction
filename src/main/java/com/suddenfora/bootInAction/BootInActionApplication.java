20fd0b881d8310ac2534cdb18eadf9126716657a
Fsud <suddenfora@gmail.com>
2018-09-30 14:49:42 +0800

    first commit

---
+++ src/main/java/com/suddenfora/bootInAction/BootInActionApplication.java
@@ -0,0 +1,12 @@
+package com.suddenfora.bootInAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootInActionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/suddenfora/bootInAction/BootInActionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.suddenfora.bootInAction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootInActionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
